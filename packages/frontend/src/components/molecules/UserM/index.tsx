250884b3ca000d9065b55988bca562a5045bfeff
TranTamdeptrai <94815751+TranTamdeptrai@users.noreply.github.com>
2025-06-04 16:51:55 +0700

    deploy_fe

--- packages/frontend/src/components/molecules/UserM/index.tsx
+++ packages/frontend/src/components/molecules/UserM/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState, useCallback } from 'react';
-import { SearchOutlined, PlusOutlined, EyeOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { SearchOutlined, PlusOutlined, EyeOutlined, EditOutlined, DeleteOutlined, UploadOutlined } from '@ant-design/icons';
 import type { InputRef, TableColumnsType, TableColumnType } from 'antd';
-import { Button, Input, Space, Table, Spin, message, Form, Modal, Select, notification } from 'antd';
+import { Button, Input, Space, Table, Spin, message, Form, Modal, Select, notification, Upload } from 'antd';
 import type { FilterDropdownProps, TableRowSelection } from 'antd/es/table/interface';
 import Highlighter from 'react-highlight-words';
 import axios from 'axios';
@@ -29,6 +29,7 @@ const UserM: React.FC = () => {
     const [currentId, setCurrentId] = useState<string | null>(null);
     const [api, contextHolder] = notification.useNotification();
     const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+    const [file, setFile] = useState<File | null>(null);
 
     useEffect(() => {
         fetchData();
@@ -55,6 +56,55 @@ const UserM: React.FC = () => {
         setIsEditing(false);
         setIsModalOpen(true);
         form.resetFields();
+    };
+
+    const handleUpload = async (pauseOnHover: boolean) => {
+        if (!file) {
+            api.error({
+                message: 'Chưa chọn file',
+                description: 'Chọn file để upload',
+                placement: 'bottomRight',
+                showProgress: true,
+                pauseOnHover,
+            })
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try {
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/upload/users`, formData, {
+                headers: { 'Content-Type': 'multipart/form-data' },
+            });
+
+            api.success({
+                message: 'Upload thành công',
+                description: `File ${file.name} đã được upload`,
+                placement: 'bottomRight',
+                showProgress: true,
+                pauseOnHover,
+            });
+
+            setFile(null);
+            fetchData();
+        } catch (error) {
+            api.error({
+                message: 'Lỗi khi Upload file',
+                placement: 'bottomRight',
+                showProgress: true,
+                pauseOnHover,
+            });
+        }
+    };
+
+    const props = {
+        accept: '.xlsx, .xls',
+        beforeUpload: (file: File) => {
+            setFile(file);
+            return false;
+        },
+        showUploadList: false,
     };
 
     const handleCancel = () => {
@@ -310,6 +360,14 @@ const UserM: React.FC = () => {
             <div className="space-x-3 flex justify-normal">
                 <Button type="primary" icon={<PlusOutlined />} onClick={showModal} style={{ marginBottom: 16 }}>
                     Tạo mới
+                </Button>
+                <Upload {...props}>
+                    <Button icon={<UploadOutlined />} className="custom-button">
+                        {file ? file.name : 'Chọn File'}
+                    </Button>
+                </Upload>
+                <Button onClick={() => handleUpload(true)} className="custom-button">
+                    Upload
                 </Button>
                 <Button
                     danger

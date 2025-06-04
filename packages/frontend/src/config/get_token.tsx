4a3c5a7b2851f125ccc8677cfffc1376db090bc3
TranTamdeptrai <94815751+TranTamdeptrai@users.noreply.github.com>
2025-06-04 14:35:05 +0700

    fix_ssr

--- packages/frontend/src/config/get_token.tsx
+++ packages/frontend/src/config/get_token.tsx
@@ -1,5 +1,13 @@
 export const getAuthConfig = () => {
-  const token =localStorage.getItem("token");
+  if (typeof window === "undefined") {
+    return {
+      headers: {
+        Authorization: "",
+      },
+    };
+  }
+
+  const token = localStorage.getItem("token");
   return {
     headers: {
       Authorization: token ? `Bearer ${token}` : "",

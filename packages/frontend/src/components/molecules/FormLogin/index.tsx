ac2e1af68680ce1c5df9a4c53751de2f4ccf6bb6
TranTamdeptrai <94815751+TranTamdeptrai@users.noreply.github.com>
2025-06-04 15:54:29 +0700

    deploy

--- packages/frontend/src/components/molecules/FormLogin/index.tsx
+++ packages/frontend/src/components/molecules/FormLogin/index.tsx
@@ -30,8 +30,10 @@ const SignIn = () => {
 
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/login`, {
             method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ code, password }),
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
         });
 
         const data = await res.json();
